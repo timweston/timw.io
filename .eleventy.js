5f8bd60f58d37d1b62f7863273b8ff6ca4e510a8
Tim Weston <tim@tim-weston.com>
2022-12-02 23:17:10 -0500

    Update .eleventy.js to include addPassthroughCopy for css directory

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 const pluginSEO = require('eleventy-plugin-seo');
 
 module.exports = function (eleventyConfig) {
+    /* Allow passthrough of CSS */
+    eleventyConfig.addPassthroughCopy('_src/css');
+
     return {
         dir: {
             input: '_src',
